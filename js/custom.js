52b4ef45b58cd7ff621d697e2d2fea9c4559d5d6
Satish Chakma <77580640+satishchakma@users.noreply.github.com>
2021-11-29 00:26:30 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,75 @@
+function one(){
+	document.getElementById('view').value += 1;
+}
+
+		function two(){
+
+		document.getElementById('view').value += 2;
+		}
+		
+		function three(){
+
+		document.getElementById('view').value += 3;
+		}
+		
+		function four(){
+
+		document.getElementById('view').value += 4;
+		}
+		
+		function five(){
+
+		document.getElementById('view').value += 5;
+		}
+		function six(){
+
+		document.getElementById('view').value += 6;
+		}
+		function seven(){
+
+		document.getElementById('view').value += 7;
+		}
+		function eight(){
+
+		document.getElementById('view').value += 8;
+		}
+		function nine(){
+
+		document.getElementById('view').value += 9;
+		}
+		function zero(){
+
+		document.getElementById('view').value += 0;
+		}
+		
+		function plus(){
+
+		document.getElementById('view').value += "+";
+		}
+		function sub(){
+
+		document.getElementById('view').value += "-";
+		}
+		function mul(){
+
+		document.getElementById('view').value += "*";
+		}
+		function divi(){
+
+		document.getElementById('view').value += "/";
+		}
+		function equal(){
+
+		document.getElementById('view').value = eval(document.getElementById('view').value);
+		}
+
+		function clr(){
+
+		document.getElementById('view').value = "";
+		}
+		function dot(){
+
+		document.getElementById('view').value += ".";
+		}
+
+		
